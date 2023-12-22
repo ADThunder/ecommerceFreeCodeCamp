64f472fe96e7682a6bf7cbeb2df8bddffb5841ab
ADThunder <maianhdung02051996@gmail.com>
2023-12-22 21:59:25 +0700

    day code

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import FormSubmitButton from "./FormSubmitButton";
+import ProductCard from "./ProductCard";
+import PriceTag from "./PriceTag";
+import PaginationBar from "./PaginationBar";
+
+export { FormSubmitButton, ProductCard, PriceTag, PaginationBar };
